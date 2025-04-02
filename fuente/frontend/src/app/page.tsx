20ced6ff2846e2805dc5f035cab7d292ec9c140f
jrub07 <jrubram07@g.educaand.es>
2025-04-02 18:10:03 +0200

    eliminada parte back, seguimos solo con react ya que es fullstack

--- fuente/frontend/src/app/page.tsx
+++ fuente/frontend/src/app/page.tsx
@@ -1,6 +1,23 @@
+'use client';
+
+import { useEffect } from 'react';
 import Image from "next/image";
+import { supabase } from "@/utils/supabase/client"; 
 
 export default function Home() {
+  useEffect(() => {
+    const fetchData = async () => {
+      const { data, error } = await supabase.from('actividades').select('*');
+      if (error) {
+        console.error("❌ Error al conectar con Supabase:", error.message);
+      } else {
+        console.log("✅ Conexión exitosa. Datos:", data);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
